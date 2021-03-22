0ba87ed6bd16928c565b789d41cad281e34ac6f1
Reno Raksi <adriano.raksi@gmail.com>
2021-03-21 22:19:52 -0500

    Implemented reading CSV file into a list of players

--- PlayerDataReader.java
+++ PlayerDataReader.java
@@ -1,12 +1,46 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.DataFormatException;
 
 public class PlayerDataReader implements PlayerDataReaderInterface {
+    private static final int PLAYER_NAME = 0;
+    private static final int REB = 14;
+    private static final int AST = 15;
+    private static final int STL = 16;
+    private static final int BLK = 17;
+    private static final int PTS = 20;
+
+    private int parseStat(String s) {
+        return s.equals("-") ? 0 : Integer.parseInt(s);
+    }
+
     @Override
     public List<PlayerInterface> readDataSet(Reader r) throws FileNotFoundException, DataFormatException, IOException {
-        return null;
+        List<PlayerInterface> players = new LinkedList<>();
+        CSVReader csvReader = new CSVReader(r);
+        csvReader.skip(1); // skipping header line
+        for (String[] line : csvReader)
+            players.add(new Player(
+                    line[PLAYER_NAME],
+                    parseStat(line[PTS]),
+                    parseStat(line[REB]),
+                    parseStat(line[BLK]),
+                    parseStat(line[STL]),
+                    parseStat(line[AST])));
+        return players;
+    }
+
+    public static void main(String[] args) throws Exception {
+        PlayerDataReader dr = new PlayerDataReader();
+        System.out.println(dr.readDataSet(new FileReader("nbaplayers.csv")));
     }
 }
