7a88b9baac5c5c0056be4f62e369433c4f66ab03
Reno Raksi <adriano.raksi@gmail.com>
2021-03-21 20:41:56 -0500

    Implemented PlayerInterface

---
+++ Player.java
@@ -0,0 +1,84 @@
+import java.util.Objects;
+
+public class Player implements PlayerInterface {
+    private String name;
+    private int points;
+    private int rebounds;
+    private int blocks;
+    private int steals;
+    private int assists;
+
+    public Player(String name, int points, int rebounds, int blocks, int steals, int assists) {
+        this.name = name;
+        this.points = points;
+        this.rebounds = rebounds;
+        this.blocks = blocks;
+        this.steals = steals;
+        this.assists = assists;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPoints() {
+        return points;
+    }
+
+    @Override
+    public int getRebounds() {
+        return rebounds;
+    }
+
+    @Override
+    public int getBlocks() {
+        return blocks;
+    }
+
+    @Override
+    public int getSteals() {
+        return steals;
+    }
+
+    @Override
+    public int getAssists() {
+        return assists;
+    }
+
+    @Override
+    public int compareTo(PlayerInterface other) {
+        return points - other.getPoints();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return points == player.points &&
+                rebounds == player.rebounds &&
+                blocks == player.blocks &&
+                steals == player.steals &&
+                assists == player.assists &&
+                Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, points, rebounds, blocks, steals, assists);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", points=" + points +
+                ", rebounds=" + rebounds +
+                ", blocks=" + blocks +
+                ", steals=" + steals +
+                ", assists=" + assists +
+                '}';
+    }
+}
