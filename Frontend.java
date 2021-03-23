dd0d922d5dd27b8f5d46a436a2c74eda216f9d45
mayan <mayan@WIN-66F8QQ2>
2021-03-22 19:55:19 -0500

    Added User Input for Search By Name Mode

--- Frontend.java
+++ Frontend.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Frontend {
 
 	private static int selectedRank = 0; 
-	private String name = ""; 
+	private static String name = ""; 
 	
 	public static void main(String[] args) {
 		Backend back = new Backend(args); 
@@ -30,12 +30,16 @@ public class Frontend {
 		searchSelectionMode(back); 
 	}
 	
-	public static void searchSelectionMode(Backend backend) {
+	/**
+	 * This acts as a Main Menu, where you can select to search by ranking or by player name
+	 * @param backend
+	 */
+	public static void searchSelectionMode(Backend backend) { // Main Menu
 		
 		@SuppressWarnings("resource")
 		Scanner user = new Scanner(System.in);  
 		// Search Selection Mode Introduction: 
-		System.out.println(" --------------------------------------------------- ");
+		System.out.println("--------------------------------------------------------------"); 
 		System.out.println("Welcome to Search Selection Mode: ");
 		System.out.println("If you want to search by All-Time NBA Points ranking, press 1.");
 		System.out.println("If you want to search by your favorite players names, press 2.");
@@ -59,7 +63,31 @@ public class Frontend {
 	}
 	
 	public static void searchByNameMode(Backend backend) {
+		name = "";  
 		
+		@SuppressWarnings("resource")
+		Scanner user = new Scanner(System.in);  
+		
+		while(true) {
+			
+			// Introduction: 
+			System.out.println("--------------------------------------------------------------"); 
+			System.out.println("Search by Player Name: ");
+			System.out.println("Please include a Player's whole name (i.e. \"Lebron James\", not \"James\""); 
+			System.out.println("If you wish to return to search selection mode, type \"exit\" "); 
+			
+			try {
+				name = user.nextLine();
+			} catch (Exception e) {
+				System.out.println("Please enter a player's whole name. Try again.");
+				searchByNameMode(backend); 
+			}
+			
+			if(name.equals("exit")) {
+				searchSelectionMode(backend); 
+			}
+			
+		}
 	}
 	
 	public static void rankMode(Backend backend) {
@@ -70,6 +98,7 @@ public class Frontend {
 		while(true) {
 			
 			// Introduction: 
+			System.out.println("--------------------------------------------------------------"); 
 			System.out.println("Search by Ranking: ");
 			System.out.println("Please enter a Rank (integer) to see the player at that ranking. Press 0 to exit back to Search Selection");
 			
